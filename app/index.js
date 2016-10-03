c9e23a33ac81e02236ddc822a1d0a60cae7a7a33
bridiver <github@brianjohnson.cc>
2016-10-02 20:06:57 -0700

    wip tabs api cc @jkup

--- app/index.js
+++ app/index.js
@@ -65,6 +65,7 @@ const contentSettings = require('../js/state/contentSettings')
 const privacy = require('../js/state/privacy')
 const basicAuth = require('./browser/basicAuth')
 const async = require('async')
+const tabs = require('./browser/tabs')
 
 // Used to collect the per window state when shutting down the application
 let perWindowState = []
@@ -391,6 +392,7 @@ app.on('ready', () => {
     Menu.init(initialState, null)
     return loadedPerWindowState
   }).then((loadedPerWindowState) => {
+    tabs.init()
     basicAuth.init()
     contentSettings.init()
     privacy.init()
